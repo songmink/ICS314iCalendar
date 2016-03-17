4c11b847f615629c7e6ae59f77392c60169605ff
Nick Winters <n.winters@hotmail.com>
2016-03-17 02:43:48 -1000

    FileReaderwithValidation

---
+++ src/ReadEvent.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReadEvent {
+	public static void main(String[] args) {
+	    String[] fileNames = GetFileNames();
+	    System.out.println(Arrays.toString(fileNames));
+	    }	    
+	public static String[] GetFileNames() {
+	  Scanner sc = new Scanner(System.in);
+	  String userinput;
+	  String checkextension;
+	  String[] FileNamesArray;
+	  File file;
+	  System.out.println("Enter ICS File Names.");
+	  userinput = sc.nextLine();
+	  FileNamesArray = userinput.split("\\s+");
+	  for (int i = 0; i < FileNamesArray.length; i++) {
+		  file = new File(FileNamesArray[i]);
+		  checkextension = FileNamesArray[i].substring(FileNamesArray[i].lastIndexOf(".") + 1, FileNamesArray[i].length());
+		  if (checkextension.equalsIgnoreCase("ics")){
+			  if (file.exists()) {
+				  return FileNamesArray;
+			  }
+		  }
+	  }		
+	  return null;
+	}
+}
