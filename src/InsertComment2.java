d08d77b89a841dae71ae621127a0da5e0a424943
lucasaec <lucaseac@hawaii.edu>
2016-03-19 05:00:54 -1000

    InsertComment2 Now checks the date of one file only, Also added a comparator
    I'm not sure how to validate the time right now.
    Please email me if something is confusing.
    I'm not sure how we want to approach this yet,
    So I wrote this for brainstorming/possibly using it for the 3rd
    deliverable. If my code is hard to read please tell me to fix something
    like add comments.

--- src/InsertComment2.java
+++ src/InsertComment2.java
@@ -1,25 +1,101 @@
 import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.io.RandomAccessFile;
+import java.util.Comparator;
 
 public class InsertComment2 {
-	
+	public static class eventCompare implements Comparator<Event> {
+		//If order is incorrect, subtract event2 from event1
+		public int compare(Event event1, Event event2) {			
+			return event1.time - event2.time;
+		}
+	} 
 	//I am unfamiliar with private static classes
 	private static class Event {
 		public String fileName;
 		public String yearMonthDay;
 		public int time;
 		public int line; // the line to removeor whateverit needs to be
-		private Event() {
-			
+		public String lastTwoLines;
+		boolean okayToAdd = false;
+		private Event(RandomAccessFile reader, String date) {
+			int readChar = -1;
+			String field = null;
+			int noOfLines = 0;
+			String dataToAdd;
+			int fieldChecks = 0; //3 if the 3 fields are present
+			try {
+			    readChar = reader.read();
+			    while(readChar != -1) {//readChar is -1 when end of file is reached, or when
+			    	                   //given incorrect data
+			    	System.out.print((char) readChar);
+			    	readChar = reader.read();
+			    	field = field + (char) readChar;
+			    	System.out.print("|" + field + "|");
+			    	if( (char) readChar == ':') {
+			    		dataToAdd = reader.readLine();
+			    		System.out.println();
+			    		if(field.equals("DTSTART:") ) { //validate and store the time
+			    			int indexOfT = dataToAdd.indexOf('T');
+			    			System.out.println(dataToAdd);
+			    			System.out.println(date);
+			    			System.out.println(dataToAdd.substring(0,indexOfT) );
+			    			if(date.equals(dataToAdd.substring(0,indexOfT) ) ) {
+			    				System.out.println(dataToAdd);
+			    		        dataToAdd = dataToAdd.substring(indexOfT+1,dataToAdd.length()-1 );
+			    		        System.out.println(dataToAdd);
+			    		        fieldChecks++; //supposed to be in if statement
+			    		        okayToAdd = true;
+			    				/*if(isValidTime(dataToAdd) ) {
+			    					time = Integer.parseInt(dataToAdd);
+			    		     		okayToAdd = true;
+			    		     		fieldChecks++;
+			    				}
+			    				else {
+			    					readChar = -1;//stop reading from file
+				    				okayToAdd = false;
+			    				}*/
+			    			}
+			    			else {
+			    				readChar = -1;//stop reading from file
+			    				okayToAdd = false;
+			    			}
+			    		}
+			    		else if(field.equals("GEO:")) {//TODO
+			    			fieldChecks++;
+			    		}
+			    		else if( (field + dataToAdd).equals("END:VEVENT")) {//TODO
+			    			line = noOfLines;//If there are too many lines
+			                                 //This will result in an error
+			    			fieldChecks++;
+			    		}
+			    		else {
+			    			
+			    		}
+			    		field = "";
+			    		noOfLines++;
+			    	}
+			    }
+			    if(fieldChecks != 3) {
+			    	okayToAdd = false;
+			    }
+			    System.out.println(fieldChecks);
+			    System.out.println(okayToAdd);
+			}
+			catch(Exception e) {
+				okayToAdd = false; //indicates that it was a badFile
+				System.out.println("ERROR");
+			}
 		}
 	}
 	
 	public static void main(String[]args) {
+		
 		ReadEvent readEvent = new ReadEvent();
 		String date;
 		String fileNames[] = null;
-		PriorityQueue queue = new PriorityQueue();
+		PriorityQueue<Event> queue;
+		queue = new PriorityQueue<Event>(5, new eventCompare() );
 		
 		System.out.println("-----------------------------------------------------");
 		System.out.println("This program will calculate the great circle distance"
@@ -36,6 +112,10 @@ public class InsertComment2 {
 	    	date = scan.nextLine();
 	    }
 	    while(!isValidDate(date));    
+	    String[] date2= date.split("/");
+	    date = date2[0] + date2[1] + date2[2];
+	    
+	    
 	    System.out.println("-----------------------------------------------------");
 	    
 		System.out.println("Now to add files");
@@ -49,13 +129,49 @@ public class InsertComment2 {
 		/* Double check if files contain valid dates and remove the ones
 		 * that do not fit.*/
 		
-		//queue.add();
+		
+		try {
+		    RandomAccessFile rfw = new RandomAccessFile(fileNames[0],"rw");
+		    Event event = new Event(rfw,date);
+		    rfw.close();
+		    if(event.okayToAdd) {
+		    	queue.add(event);
+		    	System.out.println("It worked");
+		    }
+		}
+		catch(Exception e) {
+			
+		}
+		//event.fileName = fileNames[0];
+		//event.time = getTime(rw);
+		//event.final
+		//queue.add(event);
 		
 		
 		//System.out.println(fileNames[0]+fileNames[1]);
 	}
+	/**
+	 * Time validation
+	 * @author Song Min Kim
+	 * @return boolean true if valid otherwise false
+	 */
+	public static boolean isValidTime(String t) {
+		t = t.trim();
+		String[] hm = t.split(":");
 	
+		if (hm.length != 2) {
+			return false;
+		}
 	
+		int h = Integer.parseInt(hm[0], 10);
+		int m = Integer.parseInt(hm[1], 10);
+		/* check the time validation */
+		if (0 > h || 23 < h || 0 > m || 59 < m) {
+			return false;
+		}
+		return true;
+	}
+
 	
 	/* We already know that the code below works*/
 	/**
@@ -110,25 +226,4 @@ public class InsertComment2 {
 		return true;
 	}
 	
-	/**
-	 * Time validation
-	 * @author Song Min Kim
-	 * @return boolean true if valid otherwise false
-	 */
-	public static boolean isValidTime(String t) {
-		t = t.trim();
-		String[] hm = t.split(":");
-	
-		if (hm.length != 2) {
-			return false;
-		}
-	
-		int h = Integer.parseInt(hm[0], 10);
-		int m = Integer.parseInt(hm[1], 10);
-		/* check the time validation */
-		if (0 > h || 23 < h || 0 > m || 59 < m) {
-			return false;
-		}
-		return true;
-	}
 }
