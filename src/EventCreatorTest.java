f874e3b425d2525a5767c0f7443abf47348d0d5a
Nick Winters <n.winters@hotmail.com>
2016-04-21 17:25:05 -1000

    moved Songs changes that were not synching up correctly

--- src/EventCreatorTest.java
+++ src/EventCreatorTest.java
@@ -1,157 +1,190 @@
 import static org.junit.Assert.*;
-import java.io.*;
-import java.util.Scanner;
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
 public class EventCreatorTest {
-    
+
+	// TODO E01. string reader test
 	@Test
 	public void testStringReader() {
-		EventCreator test = new EventCreator();
-		Scanner sc = new Scanner("a");
-		@SuppressWarnings("static-access")
-		String result = test.stringReader(sc);
-		assertEquals("a", result);
-	}
- 
-	@SuppressWarnings("static-access")
-	@Test //requires a test file in the ICS Calendar directory
-	public void testClassReader() throws FileNotFoundException {
-		System.out.println("------------------------");
-		System.out.println("    testClassReader     ");
-		System.out.println("------------------------");
+		String[] iData = { "a", "ab", "abc" };
+		for (int i = 0; i < iData.length; i++) {
+			Scanner sc = new Scanner(iData[i]);
+			String result = EventCreator.stringReader(sc);
+			assertEquals(iData[i], result);
+		}
 		try {
-			EventCreator eTest = new EventCreator();
-			File a = new File("classReaderScanDest.txt");
-			Scanner scan = new Scanner(a);
-			
-			System.out.println("------------------------");
-			System.out.println("  Test Case 1: Input 1  ");
-			System.out.println("------------------------");
-			String testString = eTest.classReader(scan);
-			if(!testString.equals("PUBLIC") ) {
-				fail("Given 1, it does not return PUBLIC");
-			}
-					
-			System.out.println("------------------------");
-			System.out.println("  Test Case 2: Input 2  ");
-			System.out.println("------------------------");
-			testString = eTest.classReader(scan);
-			if(!testString.equals("PRIVATE") ) {
-				fail("Given 2, it does not return PRIVATE");
-			}
-			
-			System.out.println("------------------------");
-			System.out.println("  Test Case 3: Input 3  ");
-			System.out.println("------------------------");
-			testString = eTest.classReader(scan);
-			if(!testString.equals("CONFIDENTIAL") ) {
-				fail("Given 3, it does not return CONFIDENTIAL");
-			}
-					
-			System.out.println("------------------------");
-			System.out.println("Test Case 4: Input adawda");
-			System.out.println("------------------------");
-			//enters an arbitrary String, and then 1,
-			//to check if method gets valid input
-			testString = eTest.classReader(scan);
-			scan.close();
-		}
-		catch(Exception e) {
-			System.out.println(e.getMessage() );
+			Scanner sc = new Scanner("");
+			EventCreator.stringReader(sc);
+		} catch (Exception e) {
+			// Nothing means there is an exception
 		}
 	}
-	
-	
+
+	// TODO E02. date reader test
 	@Test
 	public void testDateReader() {
-		EventCreator test = new EventCreator();
-		// Leaf year
+		// Leap year
 		Scanner sc = new Scanner("2016/02/29");
-		@SuppressWarnings("static-access")
-		String result = test.dateReader(sc);
+		String result = EventCreator.dateReader(sc);
 		assertEquals("2016/02/29", result);
 	}
 
+	// TODO E03. time reader test
 	@Test
 	public void testTimeReader() {
-		EventCreator test = new EventCreator();
-		Scanner sc = new Scanner("11:22");
-		@SuppressWarnings("static-access")
-		String result = test.timeReader(sc);
-		assertEquals("11:22", result);
-	}
-
-
-	@SuppressWarnings("static-access")
-	@Test
-	public void testIsVaildDate(){
-		EventCreator test = new EventCreator();
-		// Correct dates
-		String[] cdate = {"2016/01/31", "2016/02/29", "2016/03/31", "2016/05/31", "2016/07/31",
-				"2016/08/31", "2016/10/31", "2016/12/31", "2400/02/29"};
-		// Wrong date
-		String[] wdate = {"2016/04/31", "2016/06/31", "2016/09/31", "2016/11/31", "2015/02/29", 
-				"2100/02/29"};
-
-		for(int i = 0; i < cdate.length; i++){
-			assertTrue(test.isValidDate(cdate[i]));
-		}
-		for(int i = 0; i < wdate.length; i++){
-			assertFalse(test.isValidDate(wdate[i]));
-		}
-	}
-	
-	@SuppressWarnings("static-access")
-	@Test
-	public void testIsVaildTime(){
-		EventCreator test = new EventCreator();
 		// Correct time
-		String[] ctime = {"00:00", "01:59", "12:00"};
+		String[] ctime = { "00:00:00", "01:59:00", "12:00:00" };
 		// Wrong time
-		String[] wtime = {"24:00", "25:01", "-12:00"};
-		for(int i = 0; i < ctime.length; i++){
-			assertTrue(test.isValidTime(ctime[i]));
+		String[] wtime = { "24:00:00", "25:01:00", "-12:00:00" };
+		for (int i = 0; i < ctime.length; i++) {
+			assertTrue(EventCreator.isValidTime(ctime[i]));
 		}
-		for(int i = 0; i < wtime.length; i++){
-			assertFalse(test.isValidTime(wtime[i]));
-		}	
+		for (int i = 0; i < wtime.length; i++) {
+			assertFalse(EventCreator.isValidTime(wtime[i]));
+		}
 	}
 
-	@SuppressWarnings("static-access")
+	// TODO E04. date validation test
 	@Test
-	public void testIsValidEvent(){
-		EventCreator test = new EventCreator();
-		
-		// Correct event
-		String[][] cevent = {{"2016/01/01", "00:00", "2016/01/01", "00:01"},
-				{"2016/01/01", "01:00", "2016/01/02", "00:00"}};
-		String[][] wevent = {{"2016/01/01", "01:00", "2016/01/01", "00:00"},
-				{"2016/01/02", "12:00", "2016/01/01", "15:00"}};
-		
-		for(int i = 0; i < cevent.length; i++){
-			assertTrue(test.isValidEvent(cevent[i][0], cevent[i][1], cevent[i][2], cevent[i][3]));		
+	public void testIsVaildDate() {
+		// Correct dates
+		String[] cdate = { "2016/01/31", "2016/02/29", "2016/03/31", "2016/05/31", "2016/07/31", "2016/08/31",
+				"2016/10/31", "2016/12/31", "2400/02/29" };
+		// Wrong date
+		String[] wdate = { "2016/04/31", "2016/06/31", "2016/09/31", "2016/11/31", "2015/02/29", "2100/02/29" };
+
+		for (int i = 0; i < cdate.length; i++) {
+			assertTrue(EventCreator.isValidDate(cdate[i]));
 		}
-		
-		for(int i = 0; i < wevent.length; i++){
-			assertFalse(test.isValidEvent(wevent[i][0], wevent[i][1], wevent[i][2], wevent[i][3]));	
+		for (int i = 0; i < wdate.length; i++) {
+			assertFalse(EventCreator.isValidDate(wdate[i]));
 		}
 	}
-	
+
+	// TODO E05. time validation test
 	@Test
-	public void testClassReader2(){
-		EventCreator test = new EventCreator();
+	public void testIsVaildTime() {
+		// Correct time
+		String[] ctime = { "00:00:00", "01:59:00", "12:00:00" };
+		// Wrong time
+		String[] wtime = { "24:00:00", "25:01:00", "-12:00:00" };
+		for (int i = 0; i < ctime.length; i++) {
+			assertTrue(EventCreator.isValidTime(ctime[i]));
+		}
+		for (int i = 0; i < wtime.length; i++) {
+			assertFalse(EventCreator.isValidTime(wtime[i]));
+		}
+	}
+
+	// TODO E06. event validation test
+	@Test
+	public void testIsValidEvent() {
+		// Correct event
+		String[][] cevent = { { "2016/01/01", "00:00:00", "2016/01/01", "00:01:00" },
+				{ "2016/01/01", "01:00:00", "2016/01/02", "00:00:00" } };
+		String[][] wevent = { { "2016/01/01", "01:00:00", "2016/01/01", "00:00:00" },
+				{ "2016/01/02", "12:00:00", "2016/01/01", "15:00:00" } };
+
+		for (int i = 0; i < cevent.length; i++) {
+			assertTrue(EventCreator.isValidEvent(cevent[i][0], cevent[i][1], cevent[i][2], cevent[i][3]));
+		}
+
+		for (int i = 0; i < wevent.length; i++) {
+			assertFalse(EventCreator.isValidEvent(wevent[i][0], wevent[i][1], wevent[i][2], wevent[i][3]));
+		}
+	}
+
+	// TODO E07. class reader test
+	@Test
+	public void testClassReader() {
 		Scanner sc = new Scanner("\n");
-		String result = test.classReader2(sc);
+		String result = EventCreator.classReader(sc);
 		assertEquals("PUBLIC", result);
 
 		sc = new Scanner("p");
-		result = test.classReader2(sc);
+		result = EventCreator.classReader(sc);
 		assertEquals("PRIVATE", result);
-		
+
 		sc = new Scanner("c");
-		result = test.classReader2(sc);
+		result = EventCreator.classReader(sc);
 		assertEquals("CONFIDENTIAL", result);
+		try {
+			sc = new Scanner("fafaf");
+		} catch (Exception e) {
+			// Nothing means there is an exception
+		}
 	}
 
+	// TODO E08. latitude reader test
+	@Test
+	public void testLatReader() {
+		String testLat[] = { "-91.0f", "-90.0f", "-45.5f", "0.0f", "30.0f", "60.123456f", " 90.0f", "90.1f" };
+		for (int i = 0; i < testLat.length; i++) {
+			try {
+				Scanner sc = new Scanner(testLat[i]);
+				float result = EventCreator.latReader(sc);
+				assertEquals(testLat[i], result);
+			} catch (Exception e) {
+				// Nothing means there is an exception
+			}
+		}
+	}
+
+	// TODO E09. longitude reader test
+	@Test
+	public void testLonReader() {
+		String testLon[] = { "-191.0f", "-180.0f", "-78.6f", "0.0f", "66.0f", "60.123456f", " 180.0f", "180.1f" };
+		for (int i = 0; i < testLon.length; i++) {
+			try {
+				Scanner sc = new Scanner(testLon[i]);
+				float result = EventCreator.lonReader(sc);
+				assertEquals(testLon[i], result);
+			} catch (Exception e) {
+				// Nothing means there is an exception
+			}
+		}
+	}
+
+	// TODO E10. geo data input option
+	@Test
+	public void testMakeOptional() {
+		Scanner sc = new Scanner("1");
+		boolean result = EventCreator.makeOptional(sc, "add the geographical position of your event");
+		assertEquals(true, result);
+
+		sc = new Scanner("2");
+		result = EventCreator.makeOptional(sc, "add the geographical position of your event");
+		assertEquals(false, result);
+	}
+
+	// TODO E11. current date
+	public void testCurrentDate() {
+		SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd/HHmmss");
+		String result = EventCreator.currentDate();
+
+		// compare date
+		Date current = Calendar.getInstance().getTime();
+		String date = df.format(current);
+
+		// "T" character check
+		if (result.contains("T")) {
+			assertEquals(true, true);
+		} else {
+			assertEquals(false, true);
+		}
+
+		String r[] = result.split("T");
+		String d[] = date.split("/");
+
+		// date check
+		assertEquals(r[0], d[0]);
+		// time check (run time is under 0.05 seconds, so we can check second
+		// too.
+		assertEquals(r[1], d[1]);
+	}
 }
--- src/InsertCommentTest.java
+++ src/InsertCommentTest.java
@@ -1,67 +1,58 @@
 import static org.junit.Assert.*;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 import org.junit.Test;
 
-import static org.junit.Assert.assertThat;
-
 /**
+ * ICS 314 Spring 2016 iCalendar project
  * 
+ * 	Team Cinco: Lucas Calabrese, Nicolas Winters, Song Min Kim
+ *  Due date: 04/29/2016
  */
 
-/**
- * @author Lucas
- *
- */
 public class InsertCommentTest {
 
+	// TODO I01. event sort test
 	@Test
 	public void testSort() {
-		InsertComment test = new InsertComment();
 		String[][] events = new String[2][2];
-		
+
 		events[0][1] = "GEO:";
 		events[0][0] = "DTSTART:11111111T1111";
 		events[1][0] = "DTSTART:11111111T1110";
 		events[1][1] = "GEO:";
 		String[] fileNames = new String[2];
-		
+
 		fileNames[0] = "abc.ics";
 		fileNames[1] = "abcde.ics";
 		int[] array;
-		array = test.getSorted(events, fileNames, 2);
-		//System.out.println(array[0]);
-		assertEquals(array[0],1);
-	}
-	
-	@Test
-	public void calDistanceTest(){
-		InsertComment test = new InsertComment();
-		double d = 0;
-		double[] lat1 = {-45, 0, 45};
-		double[] lon1 = {-90, 0, 90};
-		double[] lat2 = {0, 45, 90};
-		double[] lon2 = {0,45, -45};
-		
-		int[] answer = {10010, 6672, 5004};
-		for(int i = 0; i < lat1.length; i++) {
-			Boolean max = false;
-			Boolean min = false;
-			double result = test.calDistance(lat1[i], lon1[i], lat2[i], lon2[i]);
-			int r = (int) result;
-			//System.out.println("Distance:" + r);
-			if ( r < (answer[i] + 5)){
-				max = true;
-			}
-			if ( r > (answer[i] - 5)) {
-				min = true;
-			}
-			assertEquals(max, min);
-		}
-		
+		array = InsertComment.getSorted(events, fileNames, 2);
+		// System.out.println(array[0]);
+		assertEquals(array[0], 1);
 	}
 
+	// TODO I02. great distance test
+	@Test
+	public void calDistanceTest() {
+		double[] lat1 = { -45, 0, 45 };
+		double[] lon1 = { -90, 0, 90 };
+		double[] lat2 = { 0, 45, 90 };
+		double[] lon2 = { 0, 45, -45 };
+
+		int[] answer = { 10010, 6672, 5004 };
+		for (int i = 0; i < lat1.length; i++) {
+			Boolean max = false;
+			Boolean min = false;
+			double result = InsertComment.calDistance(lat1[i], lon1[i], lat2[i], lon2[i]);
+			int r = (int) result;
+			// System.out.println("Distance:" + r);
+			if (r < (answer[i] + 5)) {
+				max = true;
+			}
+			if (r > (answer[i] - 5)) {
+				min = true;
+			}
+			assertEquals(max, true);
+			assertEquals(min, true);
+		}
+	}
 }
